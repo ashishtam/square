34a0d1c86f939d23c4e3ccdc98b5457b29bd9b2a
ashishtam <ashishtamrakar@gmail.com>
2013-04-04 09:55:01 +0545

    custom route for static page updated in application.ini and static content controller created

---
+++ application/modules/default/controllers/StaticContentController.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class StaticContentController extends Zend_Controller_Action
+{
+    public function init()
+    {
+        
+    }
+    
+    // display static views
+    public function displayAction()
+    {
+        
+        $page = $this->getRequest()->getParam('page');
+        
+        
+        if (file_exists($this->view->getScriptPath(null) .
+        "/" . $this->getRequest()->getControllerName() .
+        "/$page." . $this->viewSuffix)) 
+        {
+            $this->render($page);
+        }
+        else
+        {
+            throw new Zend_Controller_Action_Exception('Page not found', 404);
+        }
+    }
+}
